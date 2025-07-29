33bc99e3e7480c31e7bdbc1514170031ddb74633
Artolabs <artopium@gmail>
2025-07-29 13:47:54 -0600

    admin mixin fixed

--- cryptpix/integrations/admin.py
+++ cryptpix/integrations/admin.py
@@ -3,6 +3,12 @@ from django.utils.html import mark_safe
 class CryptPixAdminMixin:
     readonly_fields = ('image_layer_1_preview', 'image_layer_2_preview')
 
+    def display_thumbnail(self, obj):
+        if getattr(obj, 'image', None):
+            return mark_safe(f'<img src="{obj.image.url}" width="50" height="50" />')
+        return "No Image"
+    display_thumbnail.short_description = 'Thumbnail'
+
     def image_layer_1_preview(self, obj):
         if getattr(obj, 'image_layer_1', None):
             return mark_safe(f'<img src="{obj.image_layer_1.url}" width="200" />')
