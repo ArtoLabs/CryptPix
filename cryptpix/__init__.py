c2845b68d6a84f517edf3b3ba1bfb2225960c33a
Artolabs <artopium@gmail>
2025-07-29 14:13:25 -0600

    removed render_full_html

--- cryptpix/__init__.py
+++ cryptpix/__init__.py
@@ -1,10 +1,9 @@
 from .core import split_image_layers
-from .html import get_css, get_js, render_image_stack, render_full_html
+from .html import get_css, get_js, render_image_stack
 
 __all__ = [
     'split_image_layers',
     'get_css',
     'get_js',
     'render_image_stack',
-    'render_full_html',
 ]
