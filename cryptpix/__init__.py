ba596b8e475cad059eafc4a144d3b11f5ebddea7
Artolabs <artopium@gmail>
2025-07-29 11:58:49 -0600

    better naming

--- cryptpix/__init__.py
+++ cryptpix/__init__.py
@@ -2,7 +2,7 @@ from .core import split
 from .html import get_css, get_js, render_image_stack, render_full_html
 
 __all__ = [
-    'split',
+    'split_image_layers',
     'get_css',
     'get_js',
     'render_image_stack',
