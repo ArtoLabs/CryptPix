499e6dae49833a413131fd5d7d9c386dc1b0bf61
Artolabs <artopium@gmail>
2025-07-29 19:37:30 -0600

    javsacript responsiveness

--- cryptpix/html.py
+++ cryptpix/html.py
@@ -26,23 +26,25 @@ def get_css():
 def get_js():
     return """
 <script>
-  function resizeImageStacks() {
-    document.querySelectorAll('.image-stack').forEach(function(stack) {
-      const img = new Image();
-      img.onload = function() {
-        const aspectRatio = this.height / this.width;
-        const parentWidth = stack.parentElement.clientWidth;
-        const newHeight = parentWidth * aspectRatio;
+function resizeImageStacks() {
+  document.querySelectorAll('.image-stack').forEach(function(stack) {
+    const img = new Image();
+    img.onload = function() {
+      const aspectRatio = this.height / this.width;
+      const windowWidth = window.innerWidth;
+      const scalePercent = windowWidth / this.width;
+      const newWidth = windowWidth;
+      const newHeight = this.height * scalePercent;
 
-        stack.style.width = parentWidth + 'px';
-        stack.style.height = newHeight + 'px';
-      };
-      img.src = stack.querySelector('img').src;
-    });
-  }
+      stack.style.width = newWidth + 'px';
+      stack.style.height = newHeight + 'px';
+    };
+    img.src = stack.querySelector('img').src;
+  });
+}
 
-  window.addEventListener('DOMContentLoaded', resizeImageStacks);
-  window.addEventListener('resize', resizeImageStacks);
+window.addEventListener('DOMContentLoaded', resizeImageStacks);
+window.addEventListener('resize', resizeImageStacks);
 </script>
 """
 
