5b33e6164ecf319bfb31d746368f8b1f2742503e
Artolabs <artopium@gmail>
2025-08-02 13:01:22 -0600

    Updated admin

--- cryptpix/integrations/admin.py
+++ cryptpix/integrations/admin.py
@@ -1,25 +1,22 @@
 from django.utils.html import mark_safe
 
 class CryptPixAdminMixin:
-    readonly_fields = (
-        'image_layer_1',
-        'image_layer_2',
-        'tile_size',
-        'image_width',
-        'image_height',
-        'hue_rotation',
-        'image_layer_1_preview',
-        'image_layer_2_preview',
-    )
+    readonly_fields = ('tile_size', 'image_width', 'image_height', 'hue_rotation','image_layer_1_preview', 'image_layer_2_preview')
+
+    def display_thumbnail(self, obj):
+        if getattr(obj, 'image', None):
+            return mark_safe(f'<img src="{obj.image.url}" width="50" height="50" />')
+        return "No Image"
+    display_thumbnail.short_description = 'Thumbnail'
 
     def image_layer_1_preview(self, obj):
-        if obj.image_layer_1:
+        if getattr(obj, 'image_layer_1', None):
             return mark_safe(f'<img src="{obj.image_layer_1.url}" width="200" />')
         return "No Layer 1 Image"
     image_layer_1_preview.short_description = 'Layer 1 Preview'
 
     def image_layer_2_preview(self, obj):
-        if obj.image_layer_2:
+        if getattr(obj, 'image_layer_2', None):
             return mark_safe(f'<img src="{obj.image_layer_2.url}" width="200" />')
         return "No Layer 2 Image"
     image_layer_2_preview.short_description = 'Layer 2 Preview'
