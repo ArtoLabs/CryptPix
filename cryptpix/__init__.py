00dddadc90e0277e7dc5072f18f27beb9aec7c74
Artolabs <artopium@gmail>
2025-08-02 12:24:42 -0600

    Added filter distortion layer

--- cryptpix/__init__.py
+++ cryptpix/__init__.py
@@ -1,8 +1,9 @@
 from .core import process_and_split_image
-from .html import get_css, get_js, render_image_stack
+from .html import get_css, get_js, render_image_stack, distort_image
 
 __all__ = [
     'process_and_split_image',
+    'distort_image',
     'get_css',
     'get_js',
     'render_image_stack',
