5d764fd570c87f338fc8536218d1e36c834b0168
Artolabs <artopium@gmail>
2025-08-02 17:30:28 -0600

    moved js to its own file

--- cryptpix/__init__.py
+++ cryptpix/__init__.py
@@ -1,10 +1,9 @@
 from .core import process_and_split_image, distort_image
-from .html import get_css, get_js, render_image_stack
+from .html import get_css, render_image_stack
 
 __all__ = [
     'process_and_split_image',
     'distort_image',
     'get_css',
-    'get_js',
     'render_image_stack',
 ]
