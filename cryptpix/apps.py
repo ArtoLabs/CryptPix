dbc01548ff4d197a0b4852f6c564ff2c161fe8b4
Artolabs <artopium@gmail>
2025-08-05 06:48:13 -0600

    removed signal as its not possible

--- cryptpix/apps.py
+++ cryptpix/apps.py
@@ -3,6 +3,3 @@ from django.apps import AppConfig
 class CryptPixConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cryptpix'
-
-    def ready(self):
-        import cryptpix.signals  # Import the signals module
